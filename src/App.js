eb7a9b1c017ca07ea477d93ecff8c51b3cd5a51a
Brett O'Donnell <brett.odonnell@t4g.com>
2021-01-16 17:26:12 -0400

    Added bestiary json files. Updated packages.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import otariBestiary from './bestiaries/troubles-in-otari-bestiary.json';
 
 function App() {
-  const value = 'World';
+  var value = '';
+  console.log(otariBestiary);
+  for (let index = 0; index < otariBestiary.length; index++) {
+    const monster = otariBestiary[index];
+    value += monster.name + ", ";
+  }
+  // value = 'World';
   return <div>Hello {value}</div>;
 }
-
+ 
 export default App;
+ 
