2d81643e15a5b8888f9e24f26487344bf4482a3e
unknown <brett-o-d@hotmail.com>
2021-01-18 22:29:39 -0400

    Removed old code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import otariBestiary from './bestiaries/troubles-in-otari-bestiary.json';
 import MonsterList from './monsterList';
 
 class App extends React.Component {
   render(){
-    var value = '';
-    console.log(otariBestiary);
-    for (let index = 0; index < otariBestiary.length; index++) {
-      const monster = otariBestiary[index];
-      value += monster.name + ", ";
-    }
-    value = value;
     return <MonsterList/>;
   }
 
