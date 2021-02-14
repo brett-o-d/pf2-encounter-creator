ba33eaf7a9e7d963a73996c85289089520fd3f94
Brett O'Donnell <brett.odonnell@t4g.com>
2021-02-13 20:42:05 -0400

    Added functionality to change number of players in party

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import '../global.css';
 
 function App() {
   const [partyLevel, setPartyLevel] = useState(0);
+  const [partyCount, setPartyCount] = useState(4);
   const [difficulty, setDifficulty] = useState("");
   const [usePartyLevelAsFilter, setUsePartyLevelAsFilter] = useState(false);
   const [useXPAsFilter, setUseXPAsFilter] = useState(false);
@@ -32,8 +33,9 @@ function App() {
   // Extreme	160	        40
 
   const difficultyXP = {trivial: 40, low: 60, moderate: 80, severe: 120, extreme: 160}
+  const characterXP = {trivial: 10, low: 15, moderate: 20, severe: 30, extreme: 40}
   const encounterXP = encounterList.reduce((total, monster) => MonsterXP[monster.data.details.level.value - partyLevel] + total,0);
-  const remainingXP = difficulty === "" ? 0 : difficultyXP[difficulty] - encounterXP;
+  const remainingXP = Number.isNaN(encounterXP) ? "Invalid Encounter (Monster level out of range for party)" : (difficulty === "" ? 0 : difficultyXP[difficulty] - ((4 - partyCount) * characterXP[difficulty]) - encounterXP);
 
   return (
     <table>
@@ -43,7 +45,8 @@ function App() {
             <Filters filter={filter} setFilter={setFilter}/>
           </td>
           <td>
-            <EncounterOptions partyLevel={partyLevel} setPartyLevel={setPartyLevel} setDifficulty={setDifficulty}
+            <EncounterOptions partyLevel={partyLevel} setPartyLevel={setPartyLevel}
+              partyCount={partyCount} setPartyCount={setPartyCount} setDifficulty={setDifficulty}
               setUsePartyLevelAsFilter={setUsePartyLevelAsFilter} setUseXPAsFilter={setUseXPAsFilter}/> <br/>
             <Encounter remainingXP={remainingXP} encounterList={encounterList} partyLevel={partyLevel} encounterXP={encounterXP}
               RemoveFromEncounterList={RemoveFromEncounterList}/>
--- src/components/EncounterOptions.js
+++ src/components/EncounterOptions.js
@@ -2,12 +2,17 @@ function EncounterOptions(props) {
     
     const partyLevel = props.partyLevel;
     const setPartyLevel = props.setPartyLevel;
+    const partyCount = props.partyCount;
+    const setPartyCount = props.setPartyCount;
     const setDifficulty = props.setDifficulty;
     const setUsePartyLevelAsFilter = props.setUsePartyLevelAsFilter;
     const setUseXPAsFilter = props.setUseXPAsFilter;
 
     const minPartyLevel = 0;
     const maxPartyLevel = 29;
+    
+    const minPartyCount = 1;
+    const maxPartyCount = 8;
 
     const partyLevelChanged = (event) => {
         const value = event.target.value;
@@ -20,6 +25,21 @@ function EncounterOptions(props) {
             }
             else if (value > maxPartyLevel){
                 setPartyLevel(maxPartyLevel);
+            }
+        }
+    }
+
+    const partyCountChanged = (event) => {
+        const value = event.target.value;
+        if (partyCount !== value){
+            if (value >= minPartyCount && value <= maxPartyCount){
+                setPartyCount(value);
+            }
+            else if (value < minPartyCount) {
+                setPartyCount(minPartyCount);
+            }
+            else if (value > maxPartyCount){
+                setPartyCount(maxPartyCount);
             }
         }
     }
@@ -53,7 +73,12 @@ function EncounterOptions(props) {
                 <input type="checkbox" id="useAsFilter"  onChange={(event) => togglePartyLevelAsFilter(event)}/>
             </div>
             <br/>
-            <div id="difficulty">
+            <div className="center" id="partyCount">
+                <label htmlFor="partyCount">Number of Characters</label>
+                <input id="partyCount" value={partyCount} onChange={(event) => partyCountChanged(event)} type="number" min={minPartyCount} max={maxPartyCount}/>
+            </div>
+            <br/>
+            <div className="center" id="difficulty">
                 <p>Difficulty</p>
                 <input id="trivial" type="radio" name="difficulty" onChange={(event) => difficultyChanged(event)}/>
                 <label htmlFor="trivial">Trivial</label>
